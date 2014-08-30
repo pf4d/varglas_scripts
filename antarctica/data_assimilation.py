d9c351e0037db6e5b5d950c19e8df115062fae55
Evan Cummings <evan.cummings@umontana.edu>
2014-08-30 12:23:06 -0600

    updated antarctica

--- antarctica/data_assimilation.py
+++ antarctica/data_assimilation.py
@@ -10,7 +10,8 @@ import varglas.physical_constants as pc
 import varglas.model              as model
 from varglas.mesh.mesh_factory    import MeshFactory
 from varglas.data.data_factory    import DataFactory
-from varglas.helper               import default_nonlin_solver_params
+from varglas.helper               import default_nonlin_solver_params, \
+                                         default_config
 from varglas.utilities            import DataInput, DataOutput
 from fenics                       import *
 from time                         import time
@@ -76,7 +77,7 @@ beta_max = interpolate(Beta_max(element = model.Q.ufl_element()), model.Q)
 
 # specifify non-linear solver parameters :
 nonlin_solver_params = default_nonlin_solver_params()
-nonlin_solver_params['newton_solver']['relaxation_parameter']    = 0.8
+nonlin_solver_params['newton_solver']['relaxation_parameter']    = 0.7
 nonlin_solver_params['newton_solver']['relative_tolerance']      = 1e-3
 nonlin_solver_params['newton_solver']['maximum_iterations']      = 16
 nonlin_solver_params['newton_solver']['error_on_nonconvergence'] = False
@@ -85,84 +86,27 @@ nonlin_solver_params['newton_solver']['preconditioner']          = 'default'
 parameters['form_compiler']['quadrature_degree']                 = 2
 
 
-config = { 'mode'                         : 'steady',
-           't_start'                      : None,
-           't_end'                        : None,
-           'time_step'                    : None,
-           'output_path'                  : out_dir,
-           'wall_markers'                 : [],
-           'periodic_boundary_conditions' : False,
-           'log'                          : True,
-           'coupled' : 
-           { 
-             'on'                  : True,
-             'inner_tol'           : 0.0,
-             'max_iter'            : 1
-           },
-           'velocity' : 
-           { 
-             'on'                  : True,
-             'newton_params'       : nonlin_solver_params,
-             'viscosity_mode'      : 'full',
-             'b_linear'            : None,
-             'use_T0'              : True,
-             'use_beta0'           : False,
-             'T0'                  : model.T_w - 30.0,
-             'A0'                  : 1e-16,
-             'beta'                : None,
-             'init_beta_from_U_ob' : True,
-             'U_ob'                : U_ob,
-             'r'                   : 0.0,
-             'E'                   : 1.0,
-             'approximation'       : 'fo',
-             'boundaries'          : None,#'user_defined',
-             'u_lat_boundary'      : u,
-             'v_lat_boundary'      : v,
-             'log'                 : True
-           },
-           'enthalpy' : 
-           { 
-             'on'                  : True,
-             'use_surface_climate' : False,
-             'T_surface'           : T_s,
-             'q_geo'               : q_geo,
-             'lateral_boundaries'  : None,
-             'log'                 : True 
-           },
-           'free_surface' :
-           { 
-             'on'                  : False,
-             'lump_mass_matrix'    : True,
-             'thklim'              : thklim,
-             'use_pdd'             : False,
-             'observed_smb'        : adot,
-           },  
-           'age' : 
-           { 
-             'on'                  : False,
-             'use_smb_for_ela'     : True,
-             'ela'                 : None,
-           },
-           'surface_climate' : 
-           { 
-             'on'                  : False,
-             'T_ma'                : None,
-             'T_ju'                : None,
-             'beta_w'              : None,
-             'sigma'               : None,
-             'precip'              : None
-           },
-           'adjoint' :
-           { 
-             'alpha'               : 0.0,
-             'gamma1'              : 1.0,
-             'gamma2'              : 100.0,
-             'max_fun'             : 20,
-             'objective_function'  : 'logarithmic',
-             'bounds'              : (beta_min, beta_max),
-             'control_variable'    : model.beta,
-             'regularization_type' : 'Tikhonov'
-           }}
+config = default_config()
+config['output_path']                     = out_dir
+config['coupled']['on']                   = True
+config['coupled']['max_iter']             = 2
+config['velocity']['newton_params']       = nonlin_solver_params
+config['velocity']['viscosity_mode']      = 'full'
+config['velocity']['use_T0']              = True
+config['velocity']['use_beta0']           = False
+config['velocity']['T0'                   = model.T_w - 30.0
+config['velocity']['init_beta_from_U_ob'] = True
+config['velocity']['U_ob']                = U_ob
+config['velocity']['boundaries']          = None#'user_defined',
+config['velocity']['u_lat_boundary']      = u
+config['velocity']['v_lat_boundary']      = v
+config['enthalpy']['on']                  = True
+config['enthalpy']['T_surface']           = T_s
+config['enthalpy']['q_geo']               = q_geo
+config['age']['on']                       = False
+config['age']['use_smb_for_ela']          = True
+config['age']['ela']                      = None
+config['adjoint']['max_fun']              = 200.0
 
 if i > 0:
   config['velocity']['init_beta_from_U_ob'] = False
@@ -172,7 +116,7 @@ if i > 0:
   config['velocity']['T0']                  = dir_b + str(i-1) + '/T.xml'
 
 F = solvers.SteadySolver(model, config)
-File(out_dir + 'beta_0.pvd') << model.beta
+File(out_dir + 'beta0.pvd') << model.beta
 F.solve()
 
 params = config['velocity']['newton_params']['newton_solver']
@@ -187,6 +131,7 @@ if i % 2 == 0:
   params['relaxation_parameter']         = 1.0
   config['velocity']['viscosity_mode']   = 'linear'
   config['velocity']['eta']              = model.eta
+  config['adjoint']['surface_integral']  = 'grounded'
   config['adjoint']['alpha']             = 0
   config['adjoint']['bounds']            = (beta_min, beta_max)
   config['adjoint']['control_variable']  = model.beta
@@ -194,11 +139,9 @@ if i % 2 == 0:
 else:
   config['velocity']['use_T0']           = True
   config['velocity']['use_beta0']        = True
-  #config['velocity']['T0']               = model.T
+  config['velocity']['T0']               = model.T
   config['velocity']['beta0']            = model.beta
-  #File(dir_b + str(i-1) + '/b_shf.xml') >> model.b_shf
-  #File(dir_b + str(i-1) + '/b_gnd.xml') >> model.b_gnd
-  b_max   = 1e16
+  b_max   = 1e10
   b_min   = 0.0
   b       = project(model.b)
   model.print_min_max(b, 'b')
@@ -206,6 +149,7 @@ else:
   config['velocity']['viscosity_mode']   = 'b_control'
   config['velocity']['b_shf']            = b
   config['velocity']['b_gnd']            = b.copy()
+  config['adjoint']['surface_integral']  = 'shelves'
   config['adjoint']['alpha']             = 0
   config['adjoint']['bounds']            = (b_min, b_max)
   config['adjoint']['control_variable']  = b
@@ -222,9 +166,8 @@ File(out_dir + 'v.xml')       << project(model.v, model.Q)
 File(out_dir + 'w.xml')       << model.w 
 File(out_dir + 'beta.xml')    << model.beta
 File(out_dir + 'eta.xml')     << project(model.eta, model.Q)
-File(out_dir + 'age.xml')     << model.age
-File(out_dir + 'b_shf.xml')   << project(model.b_shf, model.Q)
-File(out_dir + 'b_gnd.xml')   << project(model.b_gnd, model.Q)
+File(out_dir + 'b_shf.pvd')   << project(model.b_shf, model.Q)
+File(out_dir + 'b_gnd.pvd')   << project(model.b_gnd, model.Q)
 
 #XDMFFile(mesh.mpi_comm(), out_dir + 'mesh.xdmf')   << model.mesh
 #
