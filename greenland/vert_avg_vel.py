fb5a8221529e4b4a4c09c7367dfe7209e41c08c4
Evan Cummings <evan.cummings@umontana.edu>
2015-01-18 18:12:52 -0700

    working scripts, updates such as they are

--- greenland/vert_avg_vel.py
+++ greenland/vert_avg_vel.py
@@ -7,47 +7,63 @@ from fenics                       import *
 # set the in/out directory :
 out_dir = 'dump/vert_avg_vel/'
 in_dir  = 'dump/test/04/'
+var_dir = 'dump/vars/'
 
-thklim  = 200.0
+mesh   = MeshFactory.get_greenland_detailed()
+#mesh   = Mesh(var_dir + 'mesh.xdmf')
+Q      = FunctionSpace(mesh, 'CG', 1)
+ff     = MeshFunction('size_t', mesh)
+cf     = MeshFunction('size_t', mesh)
+ff_acc = MeshFunction('size_t', mesh)
+S      = Function(Q)
+B      = Function(Q)
 
-# collect the raw data :
-bamber  = DataFactory.get_bamber(thklim = thklim)
+f = HDF5File(mesh.mpi_comm(), var_dir + 'vars.h5', 'r')
 
-# define the mesh :
-mesh    = MeshFactory.get_greenland_detailed()
-
-# create data objects to use with varglas :
-dbm     = DataInput(bamber,   mesh=mesh)
-
-# get the expressions used by varglas :
-S = dbm.get_expression('S',    near=True)
-B = dbm.get_expression('B',    near=True)
-M = dbm.get_expression('mask', near=True)
+f.read(ff,     'ff')
+f.read(cf,     'cf')
+f.read(ff_acc, 'ff_acc')
+f.read(S,      'S')
+f.read(B,      'B')
 
 model = model.Model()
 model.set_mesh(mesh)
-model.set_geometry(S, B, deform=True)
-model.calculate_boundaries(M)
+model.set_surface_and_bed(S, B)
+model.set_subdomains(ff, cf, ff_acc)
 model.initialize_variables()
 
-File(in_dir + 'beta.xml') >> model.beta
-File(in_dir + 'u.xml')    >> model.u
-File(in_dir + 'v.xml')    >> model.v
-File(in_dir + 'w.xml')    >> model.w
-  
-ubar = model.calc_vert_average(model.u)
-vbar = model.calc_vert_average(model.v)
-beta = model.extrude(model.beta, [3,5], 2)
+#File(in_dir + 'beta.xml') >> model.beta
+#File(in_dir + 'u.xml')    >> model.u
+#File(in_dir + 'v.xml')    >> model.v
+#File(in_dir + 'w.xml')    >> model.w
+#  
+#ubar = model.calc_vert_average(model.u)
+#vbar = model.calc_vert_average(model.v)
+#beta = model.extrude(model.beta, [3,5], 2)
+#
+#File(out_dir + 'ubar.pvd') << ubar
+#File(out_dir + 'vbar.pvd') << vbar
+#File(out_dir + 'beta.pvd') << beta
+#
+#File(out_dir + 'ubar.xml') << ubar
+#File(out_dir + 'vbar.xml') << vbar
+#File(out_dir + 'beta.xml') << beta
 
-File(out_dir + 'ubar.pvd') << ubar
-File(out_dir + 'vbar.pvd') << vbar
-File(out_dir + 'beta.pvd') << beta
+ubar = Function(Q)
+vbar = Function(Q)
+beta = Function(Q)
+
+File(out_dir + 'ubar.xml') >> ubar
+File(out_dir + 'vbar.xml') >> vbar
+File(out_dir + 'beta.xml') >> beta
 
 # write data to matlab matrix file :
-do = DataOutput(out_dir)
+bamber = DataFactory.get_bamber(thklim = 1.0)
+dbm    = DataInput(bamber, mesh=mesh)
+do     = DataOutput(out_dir)
 do.write_matlab(dbm, ubar, 'ubar')
-do.write_matlab(dbm, vbar, 'ubar')
-do.write_matlab(dbm, beta, 'ubar')
+do.write_matlab(dbm, vbar, 'vbar')
+do.write_matlab(dbm, beta, 'beta')
 
 
 
